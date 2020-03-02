f51aec2c90762c0a5a909cb0be1832f3058c2deb
Evgeny <ekiscode@gmail.com>
2020-03-02 20:21:30 +0500

    add ToDo App

--- src/components/welcomePage/WelcomePage.js
+++ src/components/welcomePage/WelcomePage.js
@@ -1,59 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 import s from "./welcomePage.module.scss";
+import { Link } from "react-router-dom";
+
+const skillItem = (title, iclass) => {
+  return (
+    <div key={title} className={s.skills__item}>
+      <p className={s.skills__item__title}>{title}</p>
+      <i className={iclass} />
+    </div>
+  );
+};
+let ini = [
+  { title: "react", iclass: `fab fa-react ${s.react}` },
+  { title: "javascript", iclass: `fab fa-js ${s.js}` },
+  { title: "sass", iclass: `fab fa-sass ${s.sass}` },
+  { title: "html5", iclass: `fab fa-html5 ${s.html5}` },
+  { title: "css", iclass: `fab fa-css3-alt  ${s.css}` },
+  { title: "github", iclass: `fab fa-github ${s.github}` },
+  { title: "git", iclass: `fab fa-git-square ${s.git}` }
+];
 
 const WelcomePage = () => {
+  const [skills] = useState(ini);
+  let skillsArr = skills.map(item => {
+    return skillItem(item.title, item.iclass);
+  });
   return (
     <section className={s.about}>
       <h1 className={s.title}>Frontend developer</h1>
       <h2 className={s.name}>Evgeny Kiselev</h2>
-      <h2 className={s.mySkills}>My skills</h2>
-      <div className={s.skills}>
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>react</p>
-          <i className={`fab fa-react ${s.react}`}></i>
-        </div>
 
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>javascript</p>
-          <i className={`fab fa-js ${s.js}`}></i>
+      <div className={s.skills__and__projects}>
+        <div className={s.skills}>
+          <h2 className={s.mySkills}>My skills</h2>
+          <div className={s.skills__items}>{skillsArr}</div>
         </div>
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>sass</p>
-          <i className={`fab fa-sass ${s.sass}`}></i>
-        </div>
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>html</p>
-          <i className={`fab fa-html5 ${s.html5}`}></i>
-        </div>
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>css</p>
-          <i className={`fab fa-css3-alt  ${s.css}`}></i>
-        </div>
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>github</p>
-          <i className={`fab fa-github ${s.github}`}></i>
-        </div>
-        <div className={s.skills__item}>
-          <p className={s.skills__item__title}>git</p>
-          <i className={`fab fa-git-square ${s.git}`}></i>
+        <div className={s.projects}>
+          <h2 className={s.mySkills}>My projects</h2>
+          <Link className={s.projectLink} to="/todoredux">
+            ToDoApp Redux
+          </Link>
+          <Link className={s.projectLink} to="/inprogress">
+            inProgress
+          </Link>
         </div>
       </div>
 
       <ul className={s.links}>
         <li className={s.link__item}>
           <a href="skype:live:ekiscode?chat">
-            <i className="fab fa-skype"></i>
+            <i className="fab fa-skype" />
           </a>
         </li>
         <li className={s.link__item}>
           <a href="mailto:ekiscode@gmail.com">
-            <i className="fas fa-at"></i>
+            <i className="fas fa-at" />
           </a>
         </li>
         <li className={s.link__item}>
           <a href="https://github.com/nomexon">
-            <i className="fab fa-github-square"></i>
+            <i className="fab fa-github-square" />
           </a>
         </li>
       </ul>
